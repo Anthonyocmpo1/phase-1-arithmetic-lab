d1d7bd4c4827ad7186caff599e2fe8074a549222
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-09-24 11:26:40 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
+const random = Math.floor(Math.random() * 100) + 1; // Random integer between 1 and 100
+const num3 = 24;
+const num4 = 5;
 
-// Write your code here
+// Change random to a fixed value if you want to control the outcome
+// const random = 20; // Uncomment this line to set random to 20
+
+const multiply = num1 * num2;
+const mod = num3 % num4;
+const max = Math.max(10, 15, 20); // This will take the highest value from the variables
+
+module.exports = { num1, num2, random, num3, num4, multiply, mod, max };
